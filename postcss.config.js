bbbbdc6f01c8d7ddc6d99166e825e369ac174baf
Alexandra Paramonova <alpar.wb@gmail.com>
2023-10-08 09:45:32 +0500

    refactor: add project settings

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+// import cssnano from "cssnano";
+import postcssNesting from "postcss-nesting";
+
+export default {
+  plugins: [
+    // cssnano({
+    //     preset: "default",
+    // }),
+    postcssNesting({
+      /* plugin options */
+    }),
+  ],
+};
