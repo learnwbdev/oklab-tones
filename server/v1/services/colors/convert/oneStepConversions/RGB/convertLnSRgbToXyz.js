1fa132c7adf154b8d51a9278beb63dbc20a48eb3
Alexandra Paramonova <alpar.wb@gmail.com>
2023-10-09 20:42:39 +0500

    feat: add functions to convert linear RGB to XYZ color space and vice versa

---
+++ server/v1/services/colors/convert/oneStepConversions/RGB/convertLnSRgbToXyz.js
@@ -0,0 +1,21 @@
+// Convert linear sRGB to CIEXYZ
+
+/**
+ * Convert linear sRGB to CIEXYZ
+ * @param {number[]} colorLinSRgb - Array of linear RGB values: r, g, b as 0..1 (red, green, blue components of the color)
+ * @return {number[]} Array of CIEXYZ values: X, Y, Z as 0..1
+ */
+
+import { lin_sRGB__to__XYZ_D65 } from '../../../matrices/colorMatrices.js';
+import { multiplyMatrices } from '../../../utils/multiplyMatrices.js';
+
+export function convertLnSRgbToXyz(colorLinSRgb) {
+    /*
+       matrices from:
+               https://github.com/facelessuser/coloraide/blob/main/coloraide/spaces/srgb_linear.py
+    */
+
+    const colorXYZ = multiplyMatrices(lin_sRGB__to__XYZ_D65, colorLinSRgb);
+
+    return colorXYZ;
+}
