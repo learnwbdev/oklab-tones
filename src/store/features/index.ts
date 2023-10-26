f543e8729c1c7ade7b494a39fa7815c551a42897
Alexandra Paramonova <alpar.wb@gmail.com>
2023-10-26 21:41:58 +0500

    fix: add export for the slices

---
+++ src/store/features/index.ts
@@ -0,0 +1,6 @@
+export { default as paletteReducer } from "./palette/palette.slice";
+export { default as paletteSettingsReducer } from "./palette/paletteSettings.slice";
+export { default as paletteBaseColorReducer } from "./palette/paletteBaseColor.slice";
+export { default as paletteActionsReducer } from "./palette/paletteActions.slice";
+export { default as themeReducer } from "./theme/theme.slice";
+export { default as langReducer } from "./lang/lang.slice";
