0e22c2b0b7572184b3b5589cca16f3ff9954fb7b
Alexandra Paramonova <alpar.wb@gmail.com>
2023-10-27 23:26:40 +0500

    feat: add home page

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,13 @@
+import Header from "../../layouts/header";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+      </main>
+    </>
+  );
+};
+
+export default HomePage;
