8a7009b6d3b4ef750a713017df8bd98d09bbd9f4
Alexandra Paramonova <alpar.wb@gmail.com>
2023-10-27 22:19:04 +0500

    fix: add exports for nav buttons and links

---
+++ src/components/navigation/index.ts
@@ -0,0 +1,4 @@
+export { default as ButtonNavMenu } from "./btn-nav-menu";
+export { default as ButtonToggleTheme } from "./btn-toggle-theme";
+export { default as ButtonToggleLang } from "./btn-toggle-lang";
+export { default as LinksNavTop } from "./links-nav-top";
