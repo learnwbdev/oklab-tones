795b4fb45d244721672abdd91d719d9bd816acfc
Alexandra Paramonova <alpar.wb@gmail.com>
2023-10-28 00:12:52 +0500

    feat: add pages routes to app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,8 +1,23 @@
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import { router } from "../..";
+import store from "../../store/store.ts";
+import styles from "./App.module.css";
+
 const App = () => {
   return (
-    <div>
+    <div className={styles["app"]}>
+      <RouterProvider router={router} />
     </div>
   );
 };
 
-export default App;
+const AppWrapper = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
+export default AppWrapper;
