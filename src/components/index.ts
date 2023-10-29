adb5003002d7ddd6c38e522fe123a9b54aab692b
Alexandra Paramonova <alpar.wb@gmail.com>
2023-10-29 14:26:00 +0500

    fix: add exports for component folders

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as App } from "./app";
+export * from "./navigation";
+export { default as Logo } from "./logo";
+export { default as FooterLInks } from "./footer-links";
+export * from "./palettes";
+export * from "./input-color";
+export { default as MsgColorCopied } from "./msg-color-copied";
