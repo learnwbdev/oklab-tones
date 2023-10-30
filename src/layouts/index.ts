1f46c99cb689c4752cbbe8a6343b1d13e5e39b68
Alexandra Paramonova <alpar.wb@gmail.com>
2023-10-30 19:54:46 +0500

    feat: add save palette section component

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -3,3 +3,4 @@ export { default as NavBar } from "./navbar";
 export { default as Header } from "./header";
 export { default as SectionPalettes } from "./section-palettes";
 export { default as SectionInputColor } from "./section-input-color";
+export { default as SectionSavePalette } from "./section-save-palette";
---
+++ src/layouts/section-save-palette/SectionSavePalette.tsx
@@ -0,0 +1,11 @@
+import { ButtonSavePalette } from "../..";
+import styles from "./SectionSavePalette.module.css";
+
+const SectionSavePalette = () => {
+  return (
+    <div className={styles.btn_save_cont}>
+      <ButtonSavePalette />
+    </div>
+  );
+};
+export default SectionSavePalette;
