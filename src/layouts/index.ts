62d4a8c4731a01d5f2e28ceeadcc73aa37ceccad
Alexandra Paramonova <alpar.wb@gmail.com>
2023-10-31 21:34:01 +0500

    fix: add article components to exports

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -4,3 +4,6 @@ export { default as Header } from "./header";
 export { default as SectionPalettes } from "./section-palettes";
 export { default as SectionInputColor } from "./section-input-color";
 export { default as SectionSavePalette } from "./section-save-palette";
+export { default as ArticlePaletteAboutTones } from "./article-palette-about-tones";
+export { default as ArticlePaletteAboutSave } from "./article-palette-about-save";
+export { default as ArticlePaletteAboutCreate } from "./article-palette-about-create";
